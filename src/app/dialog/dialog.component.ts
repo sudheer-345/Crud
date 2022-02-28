50ffdf82d09142c09cc17f76ad2d9e194824180a
sudheer -345 <sudheerreddy2929@gmail.com>
2022-02-28 19:36:39 +0530

    new project

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) { }
+  openDialog() {
+    this.dialog.open(DialogComponent);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
